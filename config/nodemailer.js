7da23f98f255b8ca803d8603be66b64855c5ab31
waqararshad786 <waqar321arshad@gmail.com>
2026-09-24 07:06:36 +0000

    new update

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -7,6 +7,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
   port: Number(process.env.EMAIL_PORT) || 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
