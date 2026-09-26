b811544263bd8ceff6a2fd19603e768d3b1becd5
waqararshad786 <waqar321arshad@gmail.com>
2026-09-26 08:17:23 +0000

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      process.env.CLIENT_URL,
+      "https://ali-hajveri-frontend-8x7c.vercel.app",
     ].filter(Boolean),
     credentials: true,
   }),
